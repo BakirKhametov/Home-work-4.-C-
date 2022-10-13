13ca162a88263911a50e2ecde83317a5bf39f357
Khametov Bakir <bakir_90@inbox.ru>
2022-10-13 22:34:04 +0300

    add task1

---
+++ home task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]*/
+
+Console.Clear();
+int[] array = new int[8];
+void array1 (int[] array)
+{
+int length = array.Length;
+for (int i = 0; i < length; i++)
+{
+    
+    
+    array[i] = new Random().Next(0,999);
+    Console.Write($"{array[i]} | ");
+
+
+}
+
+}
+array1(array);
