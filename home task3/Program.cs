dae03a8db538c90935d056259d8ec1a98a276a32
Khametov Bakir <bakir_90@inbox.ru>
2022-10-15 16:15:30 +0300

    add task3

---
+++ home task3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(number));
+
+int SumNumbers (int num)
+{
+    int Sum = 0;
+    while (number>0)
+    {
+        Sum = (number %10) + Sum;
+        number = number/10;
+    }
+    return Sum;
+    //Console.WriteLine(Sum);
+
+}
